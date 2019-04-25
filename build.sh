9b4797eceac45a415f8e71782dd4a4d04e708be7
Ryan Stuart <rstuart@scottlogic.com>
2019-04-25 16:50:01 +0100

    701 Update circleCI and bash script

--- build.sh
+++ build.sh
@@ -3,6 +3,20 @@ if [[ "$#" -ne 1 ]]; then
     echo "Usage: build.sh version"
     exit
 fi
+
+# Get latest tag, assume this is the version.
+CURRENT="$(git describe --tags --abbrev=0)"
+
+echo ${CURRENT}
+
+# Update the version based on the history
+
+echo "$sql"
+
+HISTORY="$(git log ${CURRENT}..HEAD --pretty="%s")"
+
+echo ${HISTORY}
+
 mvn -B -DdryRun=true -DreleaseVersion=$1 -DdevelopmentVersion=$1-SNAPSHOT -DautoVersionSubmodules=true release:clean release:prepare
 SRC=datahelix-src-$1
 cd ..
